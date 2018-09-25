787fe04508f29bcfdf0a1aab0208663c44359105
ian.agung@len.co.id <agung220460>
2018-09-25 09:36:46 +0700

    * LearnCsub.sln: Add new Project - publish
    * Program.cs:
    * LogExample.csproj:
    * SwitchCase.csproj:
    * XmlComment.csproj:
    * AssemblyInfo.cs:

---
+++ LogExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
