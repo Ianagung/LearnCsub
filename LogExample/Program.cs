f1d885bd5716fc007159f2e1b8b3a765b437cb76
ian.agung@len.co.id <agung220460>
2018-10-09 13:35:32 +0700

    Add sample debug , error

--- LogExample/Program.cs
+++ LogExample/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using System.Reflection;
+using log4net;
+using log4net.Config;
 namespace LogExample
 {
     class MainClass
     {
+        private static readonly ILog _log = 
+            LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XmlConfigurator.Configure();
+            var displayMsg = "Hello World!";
+            var error = "Error Cuy, ?????";
+            var debug = "Hello, saatnya kita debug";
+            _log.Info(displayMsg);
+            _log.Error(error);
+            _log.Debug(debug);
+            Console.WriteLine(displayMsg);
             Console.WriteLine("Tes");
         }
     }
